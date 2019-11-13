9930f6c7956ea4e7120963bc79eb15f04cc0fa0b
Catherine Olejarczyk <Colejarczyk6866@conestogac.on.ca>
2019-11-13 15:15:07 -0500

    created test 1 and test 2 surverys

--- test1/index.js
+++ test1/index.js
@@ -1,61 +1,203 @@
 
 Survey
-    .StylesManager
-    .applyTheme("default");
+.StylesManager
+.applyTheme("default");
 
 var json = {
-    title: "American History",
-    pages: [
-        {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "civilwar",
-                    title: "When was the Civil War?",
-                    choices: [
-                        "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
-                    ],
-                    correctAnswer: "1850-1900"
-                }
-            ]
-        }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "libertyordeath",
-                    title: "Who said 'Give me liberty or give me death?'",
-                    choicesOrder: "random",
-                    choices: [
-                        "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
-                    ],
-                    correctAnswer: "Patrick Henry"
-                }
-            ]
-        }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "magnacarta",
-                    title: "What is the Magna Carta?",
-                    choicesOrder: "random",
-                    choices: [
-                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
-                    ],
-                    correctAnswer: "The foundation of the British parliamentary system"
-                }
-            ]
-        }
-    ],
-    completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
+title: "Software developer survey.",
+pages: [
+    {
+        title: "What operating system do you use?",
+        questions: [
+            {
+                type: "checkbox",
+                name: "opSystem",
+                title: "OS",
+                hasOther: true,
+                isRequired: true,
+                choices: ["Windows", "Linux", "Macintosh OSX"]
+            }
+        ]
+    }, {
+        title: "What language(s) are you currently using?",
+        questions: [
+            {
+                type: "checkbox",
+                name: "langs",
+                title: "Please select from the list",
+                colCount: 4,
+                isRequired: true,
+                choices: [
+                    "Javascript",
+                    "Java",
+                    "Python",
+                    "CSS",
+                    "PHP",
+                    "Ruby",
+                    "C++",
+                    "C",
+                    "Shell",
+                    "C#",
+                    "Objective-C",
+                    "R",
+                    "VimL",
+                    "Go",
+                    "Perl",
+                    "CoffeeScript",
+                    "TeX",
+                    "Swift",
+                    "Scala",
+                    "Emacs Lisp",
+                    "Haskell",
+                    "Lua",
+                    "Clojure",
+                    "Matlab",
+                    "Arduino",
+                    "Makefile",
+                    "Groovy",
+                    "Puppet",
+                    "Rust",
+                    "PowerShell"
+                ]
+            }
+        ]
+    },
+    {
+        title: "How long have you been programming for?",
+        questions: [
+            {
+                type: "checkbox",
+                name: "years",
+                title: "Please select from the list",
+                isRequired: true,
+                choices: [
+                    "Less than 6 months",
+                    "1 year to less than 3 years",
+                    "3 years to less than 5 years",
+                    "5 years or more"
+                ]
+            }
+        ]
+    },
+    {
+        title: "Are you a student?",
+        questions: [
+            {
+                type: "checkbox",
+                name: "student",
+                title: "Please select from the list",
+                isRequired: true,
+                hasOther: true,
+                choices: [ "Yes", "No"]
+            }
+        ]
+    },
+    {
+        title: "How much time do you spend on a computer each day?",
+        questions: [
+            {
+                type: "checkbox",
+                name: "time",
+                title: "Please select from the list",
+                isRequired: true,
+                hasOther: true,
+                choices: [
+                    "Less than 3 hours",
+                    "3 hours to less than 6 hours",
+                    "All day"
+                ]
+            }
+        ]
+    },
+    {
+        title: "How often do you take breaks from the computer",
+        questions: [
+            {
+                type: "checkbox",
+                name: "breaks",
+                title: "Please select from the list",
+                isRequired: true,
+                hasOther: true,
+                choices: [
+                    "Often",
+                    "Every 2 hours",
+                    "I don't. I just work until I'm done my assignment"
+                ]
+            }
+        ]
+    },
+    {
+        title: "What do you like to do outside of work?",
+        questions: [
+            {
+                type: "text",
+                name: "Answer",
+                title: "Please put your answer in the space provided:"
+            }
+        ]
+    },
+    {
+        title: "What programming software do you work with?",
+        questions: [
+            {
+                type: "checkbox",
+                name: "software",
+                title: "Please select from the list",
+                isRequired: true,
+                hasOther: true,
+                choices: [
+                    "Visual Studio",
+                    "Visual Studio Code",
+                    "Notpad++",
+                    "Android Studio"
+                ]
+            }
+        ]
+    },
+    {
+        title: "What time do you typically wake up in the morning?",
+        questions: [
+            {
+                type: "checkbox",
+                name: "wakeup",
+                title: "Please select from the list",
+                isRequired: true,
+                hasOther: true,
+                choices: [
+                    "6 AM",
+                    "7 AM",
+                    "Sometime before 10 AM",
+                    "When I feel like it"
+                ]
+            }
+        ]
+    }, {
+        title: "Please enter your name and e-mail",
+        questions: [
+            {
+                type: "text",
+                name: "name",
+                title: "Name:"
+            }, {
+                type: "text",
+                name: "email",
+                title: "Your e-mail"
+            }
+        ]
+    }
+]
 };
 
 window.survey = new Survey.Model(json);
 
 survey
-    .onComplete
-    .add(function (result) {
-        document
-            .location = "indexthanks.html";
-    });
+.onComplete
+.add(function (result) {
+    document
+        .querySelector('#surveyResult')
+        .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
+});
 
-$("#surveyElement").Survey({ model: survey });
+survey.showProgressBar = 'bottom';
+
+$("#surveyElement").Survey({model: survey});
--- test2/single.js
+++ test2/single.js
@@ -4,41 +4,141 @@ Survey
     .applyTheme("default");
 
 var json = {
-    title: "American History",
+    title: "Software developer survey.",
     pages: [{
         questions: [
             {
-                type: "radiogroup",
-                name: "civilwar",
-                title: "When was the Civil War?",
-                choices: [
-                    "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
-                ],
-                correctAnswer: "1850-1900"
+                type: "checkbox",
+                name: "opSystem",
+                title: "What operating system do you use?",
+                hasOther: true,
+                isRequired: true,
+                choices: ["Windows", "Linux", "Macintosh OSX"]
             },
             {
-                type: "radiogroup",
-                name: "libertyordeath",
-                title: "Who said 'Give me liberty or give me death?'",
-                choicesOrder: "random",
+                type: "checkbox",
+                name: "langs",
+                title: "What language(s) are you currently using?",
+                colCount: 4,
+                isRequired: true,
                 choices: [
-                    "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
-                ],
-                correctAnswer: "Patrick Henry"
+                    "Javascript",
+                    "Java",
+                    "Python",
+                    "CSS",
+                    "PHP",
+                    "Ruby",
+                    "C++",
+                    "C",
+                    "Shell",
+                    "C#",
+                    "Objective-C",
+                    "R",
+                    "VimL",
+                    "Go",
+                    "Perl",
+                    "CoffeeScript",
+                    "TeX",
+                    "Swift",
+                    "Scala",
+                    "Emacs Lisp",
+                    "Haskell",
+                    "Lua",
+                    "Clojure",
+                    "Matlab",
+                    "Arduino",
+                    "Makefile",
+                    "Groovy",
+                    "Puppet",
+                    "Rust",
+                    "PowerShell"
+                ]
             },
             {
-                type: "radiogroup",
-                name: "magnacarta",
-                title: "What is the Magna Carta?",
-                choicesOrder: "random",
+                type: "checkbox",
+                name: "years",
+                title: "How long have you been programming for?",
+                isRequired: true,
                 choices: [
-                    "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
-                ],
-                correctAnswer: "The foundation of the British parliamentary system"
+                    "Less than 6 months",
+                    "1 year to less than 3 years",
+                    "3 years to less than 5 years",
+                    "5 years or more"
+                ]
+            },
+            {
+                type: "checkbox",
+                name: "student",
+                title: "Are you a student?",
+                isRequired: true,
+                hasOther: true,
+                choices: [ "Yes", "No"]
+            },
+            {
+                type: "checkbox",
+                name: "time",
+                title: "How much time do you spend on a computer each day?",
+                isRequired: true,
+                hasOther: true,
+                choices: [
+                    "Less than 3 hours",
+                    "3 hours to less than 6 hours",
+                    "All day"
+                ]
+            },
+            {
+                type: "checkbox",
+                name: "breaks",
+                title: "How often do you take breaks from the computer",
+                isRequired: true,
+                hasOther: true,
+                choices: [
+                    "Often",
+                    "Every 2 hours",
+                    "I don't. I just work until I'm done my assignment"
+                ]
+            },
+            {
+                type: "text",
+                name: "Answer",
+                title: "What do you like to do outside of work?"
+            },
+            {
+                type: "checkbox",
+                name: "software",
+                title: "What programming software do you work with?",
+                isRequired: true,
+                hasOther: true,
+                choices: [
+                    "Visual Studio",
+                    "Visual Studio Code",
+                    "Notpad++",
+                    "Android Studio"
+                ]
+            },
+            {
+                type: "checkbox",
+                name: "wakeup",
+                title: "What time do you typically wake up in the morning?",
+                isRequired: true,
+                hasOther: true,
+                choices: [
+                    "6 AM",
+                    "7 AM",
+                    "Sometime before 10 AM",
+                    "When I feel like it"
+                ]
+            },
+            {
+                type: "text",
+                name: "name",
+                title: "Name:",
+                type: "text",
+                name: "email",
+                title: "Your e-mail"
             }
         ]
     }],
-    completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
 };
 
 window.survey = new Survey.Model(json);
